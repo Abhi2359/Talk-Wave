2a445c2861dbcb45b83799f58114d9afb89bbb17
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-08-26 23:53:47 +0530

    chatApi

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const { protect } = require('../middlewares/authMiddleware');
-const { accessChat } = require('../controllers/chatController');
+const express = require("express");
+const { protect } = require("../middlewares/authMiddleware");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} = require("../controllers/chatController");
 
-const router = express.Router()
+const router = express.Router();
 
- router.route('/').post(protect, accessChat );
-// router.route("/").get(protect,fetchChats)
-// router.route("/group").get(protect,createGroupChat)
-// router.route("/rename").get(protect,renameGroup)
-// router.route("/groupremove").get(protect,removeFromGroup)
-// router.route("/groupadd").get(protect,addToGroup)
+router.route("/").post(protect, accessChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+ router.route("/groupremove").delete(protect,removeFromGroup)
+ router.route("/groupadd").get(protect,addToGroup)
 
-module.exports= router;
+module.exports = router;
