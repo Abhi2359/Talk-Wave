c1d562107246efd467587d7f288d735b991657df
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-09-19 23:37:27 +0530

    scrollableChat

---
+++ .history/front-end/src/components/ScrollableChat_20230913084501.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ScrollableFeed from "react-scrollable-feed";
+import { isLastMessage, isSameSender } from "../config/ChatLogics";
+import { Avatar, Tooltip } from "@chakra-ui/react";
+import { ChatState } from "../Context/ChatProvider";
+const ScrollableChat = ({ messages }) => {
+  const { user } = ChatState();
+  return (
+    <div style={{ overflowX: "hidden", overflowY: "auto" }}>
+
+        {messages &&
+          messages.map((m, i) => (
+            <div style={{ display: "flex" }} key={m._id}>
+              {(isSameSender(messages, m, i, user._id) ||
+                isLastMessage(messages, i, user._id)) && (
+                <Tooltip
+                  label={m.sender.name}
+                  placement="bottom-start"
+                  hasArrow
+                >
+                    <Avatar
+                    mt="7px"
+                    mr={1}
+                    size="sm"
+                    cursor="pointer"
+                    name={m.sender.name}
+                    src={m.sender.pic}
+                  />
+                </Tooltip>
+              )}
+            </div>
+          ))}
+    </div>
+
+  );
+};
+
+export default ScrollableChat;
